a495f479210cd2391b09e8943aeacb5c618d23e0
bluehood <blue@lcm.mi.infn.it>
2016-10-12 00:45:54 +0200

    substitute " with ' in string literals declarations

--- LCMbot.py
+++ LCMbot.py
@@ -11,16 +11,16 @@ logging.basicConfig(format=fmt, level=logging.INFO)
 
 def main():
     # create LCMbot
-    updater = Updater("TOKEN")
+    updater = Updater('TOKEN')
     dp = updater.dispatcher
 
     # register handlers
-    dp.add_handler(CommandHandler("pinglcm", hnd.ping))
-    dp.add_handler(CommandHandler("checkwebsite", hnd.is_web_up))
-    dp.add_handler(CommandHandler("vietnam", hnd.vietnam))
-    dp.add_handler(CommandHandler("sellyourmother", hnd.sell_your_mother))
-    dp.add_handler(CommandHandler("storytime", hnd.tell_a_tale))
-    dp.add_handler(CommandHandler("abuse150", hnd.abuse_150))
+    dp.add_handler(CommandHandler('pinglcm', hnd.ping))
+    dp.add_handler(CommandHandler('checkwebsite', hnd.is_web_up))
+    dp.add_handler(CommandHandler('vietnam', hnd.vietnam))
+    dp.add_handler(CommandHandler('sellyourmother', hnd.sell_your_mother))
+    dp.add_handler(CommandHandler('storytime', hnd.tell_a_tale))
+    dp.add_handler(CommandHandler('abuse150', hnd.abuse_150))
     dp.add_error_handler(hnd.error)
     # dp.add_handler(MessageHandler([], hnd.print_msg_info))
 
